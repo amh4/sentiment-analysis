ff4c5ea3a1e1d3dcb302149381377130f35e81b0
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-03-22 17:30:58 +0000

    fixed bug in mocking using @patch

--- api/tests/test_get_sentiment.py
+++ api/tests/test_get_sentiment.py
@@ -7,25 +7,23 @@ class TestSentimentAnalysis(unittest.TestCase):
   @patch('lib.get_sentiment.SentimentIntensityAnalyzer')
   def test_get_sentiment(self, mock_sia):
       mock_sia_instance = mock_sia.return_value
-      mock_sia_instance.polarity_scores.return_value = {'compound': 0.5}
+      mock_sia_instance.polarity_scores.return_value = {'neg': 0.0, 'neu': 0.295, 'pos': 0.705, 'compound': 0.8012}
       
       sentence = "This is a test sentence"
       result = get_sentiment(sentence)
       
       mock_sia.assert_called_once_with()
       mock_sia_instance.polarity_scores.assert_called_once_with(sentence)
-      self.assertEqual(result, '0.5')
+      self.assertEqual(result, '{"neg": 0.0, "neu": 0.295, "pos": 0.705, "compound": 0.8012}')
 
   @patch('lib.get_sentiment.SentimentIntensityAnalyzer')
   def test_get_sentiment2(self, mock_sia):
       mock_sia_instance = mock_sia.return_value
-      mock_sia_instance.polarity_scores.return_value = {'compound': 0.7}
+      mock_sia_instance.polarity_scores.return_value = {'neg': 0.0, 'neu': 0.295, 'pos': 0.705, 'compound': 0.99}
       
       sentence = "This is a better test sentence"
       result = get_sentiment(sentence)
-      
+      print(result)
       mock_sia.assert_called_once_with()
       mock_sia_instance.polarity_scores.assert_called_once_with(sentence)
-      self.assertEqual(result, '0.7')
-
-
+      self.assertEqual(result, '{"neg": 0.0, "neu": 0.295, "pos": 0.705, "compound": 0.99}')
