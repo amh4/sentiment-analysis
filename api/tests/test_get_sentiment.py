1193ccc239ff085a6c8027ec4d3ca594096c4352
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-03-22 17:19:05 +0000

    two tests passed, test triangulation

--- api/tests/test_get_sentiment.py
+++ api/tests/test_get_sentiment.py
@@ -1,17 +1,31 @@
+import unittest
 from unittest.mock import patch
 from lib.get_sentiment import *
 
-@patch('requests.get')
+class TestSentimentAnalysis(unittest.TestCase):
 
-def test_get_sentiment(mock_request):
-  mock_response = {
-    'neg': 0.0, 
-    'neu': 0.295, 
-    'pos': 0.705, 
-    'compound': 0.8012
-  }
-  mock_request.return_value.status_code = 200
-  mock_request.return_value.json.return_value = mock_response
+  @patch('lib.get_sentiment.SentimentIntensityAnalyzer')
+  def test_get_sentiment(self, mock_sia):
+      mock_sia_instance = mock_sia.return_value
+      mock_sia_instance.polarity_scores.return_value = {'compound': 0.5}
+      
+      sentence = "This is a test sentence"
+      result = get_sentiment(sentence)
+      
+      mock_sia.assert_called_once_with()
+      mock_sia_instance.polarity_scores.assert_called_once_with(sentence)
+      self.assertEqual(result, '0.5')
 
-  sentiment_score = get_sentiment('I have had a lovely day')
-  assert sentiment_score == {'compound': 0.8012}
+  @patch('lib.get_sentiment.SentimentIntensityAnalyzer')
+  def test_get_sentiment2(self, mock_sia):
+      mock_sia_instance = mock_sia.return_value
+      mock_sia_instance.polarity_scores.return_value = {'compound': 0.7}
+      
+      sentence = "This is a better test sentence"
+      result = get_sentiment(sentence)
+      
+      mock_sia.assert_called_once_with()
+      mock_sia_instance.polarity_scores.assert_called_once_with(sentence)
+      self.assertEqual(result, '0.7')
+
+
