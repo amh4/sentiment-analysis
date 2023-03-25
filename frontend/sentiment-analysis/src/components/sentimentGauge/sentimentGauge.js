4a8bd01c3fd65299a4c06718f54bce6004f82e0d
Rachel Newby <rachelcnewby@gmail.com>
2023-03-25 16:40:53 +0000

    CSS to gauge component

--- frontend/sentiment-analysis/src/components/sentimentGauge/sentimentGauge.js
+++ frontend/sentiment-analysis/src/components/sentimentGauge/sentimentGauge.js
@@ -7,7 +7,12 @@ const SentimentGauge = ({ sentenceScore }) => {
     <div>
       <GaugeChart id='gauge-chart1'
       nrOfLevels={20}
-      percent={sentenceScore}/>
+      colors={["#FF0000",   "#FF2200",   "#FF4400",   "#FF6600",   "#FF8800",   "#FFAA00",   "#FFCC00",   "#FFE000",   "#FFF200",   "#FCFF00",   "#D5FF00",   "#ADFF00",   "#85FF00",   "#5DFF00",   "#35FF00",   "#0DFF00",   "#00FF19",   "#00FF3B",   "#00FF5D",   "#00FF7F"]}
+      needleColor="#ff9966"
+      needleBaseColor="#ff9966"
+      hideText={true}
+      percent={(sentenceScore + 1) / 2}
+      />
     </div>
   );
 };
--- frontend/sentiment-analysis/src/components/userInput/userInput.js
+++ frontend/sentiment-analysis/src/components/userInput/userInput.js
@@ -2,17 +2,11 @@ import React, { useState } from "react";
 import SentimentGauge from "../sentimentGauge/sentimentGauge";
 
 const UserInput = () => {
-  // const [userSentence, setUserSentence] = useState(null);
-  const [sentenceScore, setSentenceScore] = useState(0.5);
-
-  // const handleInputChange = (event) => {
-  //   setUserSentence(event.target.value);
-  // };
+  const [sentenceScore, setSentenceScore] = useState(0);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const userInput = document.getElementById('input-field').value
-    // setUserSentence(userInput)
 
     fetch(
       "http://127.0.0.1:5000/get_sentiment/" + encodeURIComponent(userInput)
@@ -24,9 +18,8 @@ const UserInput = () => {
 
   return (
     <div className="user-input-form">
-      <p>Enter your sentence here:</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" id="input-field"/>
+        <input type="text" id="input-field" placeholder="Enter a sentence"/>
         <button type="submit">Submit</button>
       </form>
       <SentimentGauge sentenceScore={sentenceScore} />
