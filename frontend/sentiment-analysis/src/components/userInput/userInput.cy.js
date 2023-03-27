b63634c8c0f55ec8fb0fbb3230c318610c56f2c1
Rachel Newby <rachelcnewby@gmail.com>
2023-03-27 18:22:27 +0100

    Tests to check UserInput renders a gauge chart

--- frontend/sentiment-analysis/src/components/userInput/userInput.cy.js
+++ frontend/sentiment-analysis/src/components/userInput/userInput.cy.js
@@ -4,5 +4,10 @@ describe("UserInput component", () => {
   it("should render a form to input text", () => {
     cy.mount(<UserInput/>);
     cy.get('.user-input-form').should('be.visible')
+  });
+
+  it("should render a SentimentGauge component", () => {
+    cy.mount(<UserInput/>);
+    cy.get('#gauge-chart1').should('be.visible')
   })
 })
