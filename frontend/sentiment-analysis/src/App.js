cb9ea3ee2995af129d3c2adcf799e270d68efef4
Rachel Newby <rachelcnewby@gmail.com>
2023-03-30 12:33:14 +0100

    Text added to explain how to use website with CSS

--- frontend/sentiment-analysis/src/App.js
+++ frontend/sentiment-analysis/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import UserInput from "./components/userInput/userInput";
 
@@ -6,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <img className="sent-ence-iment-logo" src='sent-ence-iment-logo.png' alt="logo"/>
+      <p className="intro-text">Are you happy-go-lucky or more of a Debbie-Downer? Not sure? Well, now you can be; by finding out the sentiment of your sentence with Sent-Ence-Iment!<br/>
+      <br/>
+      Enter your sentence, click Submit, and Sent-Ence-Iment will rate your attitude.</p>
       <UserInput />
     </div>
   );
