03ae7a4884aec76fb30efd1c0ae3f4bb57a67d7a
Rachel Newby <rachelcnewby@gmail.com>
2023-03-30 12:01:29 +0100

    CSS added to gauge chart

--- frontend/sentiment-analysis/src/App.js
+++ frontend/sentiment-analysis/src/App.js
@@ -5,7 +5,7 @@ import UserInput from "./components/userInput/userInput";
 function App() {
   return (
     <div className="App">
-      <img src='sent-ence-iment-logo.png' alt="logo"/>
+      <img className="sent-ence-iment-logo" src='sent-ence-iment-logo.png' alt="logo"/>
       <UserInput />
     </div>
   );
--- frontend/sentiment-analysis/src/components/sentimentGauge/sentimentGauge.js
+++ frontend/sentiment-analysis/src/components/sentimentGauge/sentimentGauge.js
@@ -1,15 +1,17 @@
 import React, { useState} from "react";
 import GaugeChart from 'react-gauge-chart';
+import './sentimentGauge.css'
 
 const SentimentGauge = ({ sentenceScore }) => {
 
   return (
-    <div>
+    <div className="gague-chart-container">
       <GaugeChart id='gauge-chart1'
       nrOfLevels={20}
       colors={["#FF0000",   "#FF2200",   "#FF4400",   "#FF6600",   "#FF8800",   "#FFAA00",   "#FFCC00",   "#FFE000",   "#FFF200",   "#FCFF00",   "#D5FF00",   "#ADFF00",   "#85FF00",   "#5DFF00",   "#35FF00",   "#0DFF00",   "#00FF19",   "#00FF3B",   "#00FF5D",   "#00FF7F"]}
       needleColor="#ff9966"
       needleBaseColor="#ff9966"
+      arcWidth={0.23}
       hideText={true}
       percent={(sentenceScore + 1) / 2}
       />
