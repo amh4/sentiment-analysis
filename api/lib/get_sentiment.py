1ae76b482ad9b2b75e302772fffc7c5e0a7b41e7
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-04-04 14:08:46 +0100

    updated cors policy

--- api/lib/get_sentiment.py
+++ api/lib/get_sentiment.py
@@ -13,7 +13,8 @@ def get_sentiment(sentence):
   sia = SentimentIntensityAnalyzer()
   scores = sia.polarity_scores(sentence)
   print(json.dumps(scores))
-  return json.dumps(scores)
+  return json.dumps(scores), 200, {'Access-Control-Allow-Origin': 'https://sentiment-analysis-three.vercel.app'}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
