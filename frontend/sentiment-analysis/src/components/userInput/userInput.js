31d69a1fc6e95a65a55dfb03ea996689b8d798f9
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-04-04 12:28:45 +0100

    updated port to match render port

--- frontend/sentiment-analysis/src/components/userInput/userInput.js
+++ frontend/sentiment-analysis/src/components/userInput/userInput.js
@@ -11,9 +11,7 @@ const UserInput = () => {
     event.preventDefault();
     const userInput = document.getElementById("input-field").value;
 
-    fetch(
-      "http://127.0.0.1:5000/get_sentiment/" + encodeURIComponent(userInput)
-    )
+    fetch("http://0.0.0.0:10000/get_sentiment/" + encodeURIComponent(userInput))
       .then((response) => response.json())
       .then((data) => setSentenceScore(data.compound))
       .catch((error) => {
