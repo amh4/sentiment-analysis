e9b8a7944243dd6bdce634e4feb1dbc99350bf35
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-04-04 12:32:22 +0100

    removed cors

--- api/lib/get_sentiment.py
+++ api/lib/get_sentiment.py
@@ -6,7 +6,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=['https://sentiment-analysis-three.vercel.app/'])
+# CORS(app, origins=['https://sentiment-analysis-three.vercel.app/'])
 
 @app.route('/get_sentiment/<string:sentence>')
 def get_sentiment(sentence):
